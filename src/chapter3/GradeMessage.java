8948934fc6d2bbdfdcfec3b0a86c4741d0fe70ba
Majidaghha <mguluzada7765@ada.edu.az>
2025-05-11 15:40:27 +0200

    new1

--- src/chapter3/GradeMessage.java
+++ src/chapter3/GradeMessage.java
@@ -1,6 +1,38 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class GradeMessage {
+    public static void  main(String [] arg){
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+        String grade =  scanner.next();
+        String message;
+        switch (grade){
+            case "A":
+                message = "Excellent job";
+                break;
+            case "B":
+                message = "Great job";
+                break;
+            case "C":
+                message = "Good Job";
+                break;
+            case "D":
+                message = "You need to work a bit harder";
+                break;
+            case "F":
+                message = "Oh no!";
+                break;
+            default:
+                message = "Error, Invalid input ";
+
+
+        }
+        System.out.println(message);
+
+
+    }
 }
-//salam necesen mecid
+
 
